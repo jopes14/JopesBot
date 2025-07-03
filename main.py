8b52cc64d0ffd05a667767fa8e1854d7bce4f52a
Jopes <61479990+jopes14@users.noreply.github.com>
2025-07-03 14:01:35 -0500

    Update main.py
    Fixed the channel redirect being sent at incorrect times

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-﻿import discord
+import discord
 import random
 from discord.ext import commands
 from Data import GetCharactersNames, GetGlidersNames, GetKartsNames, GetWheelsNames, CharacterData, KartData, WheelData, GliderData
@@ -10,7 +10,7 @@ intents.presences = True
 bot = commands.Bot(activity=activity, intents=intents)
 
 TOKEN = '' # welcome to todays episode of jopes accidentally publishes his bot token to github again
-class MyView(discord.ui.View):  # Create a class called MyView that subclasses discord.ui.View
+class MyView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
     
@@ -63,21 +63,12 @@ async def on_message(message): #AC EMBED SENDER HERE
         await message.channel.send('Waffles bad')
     if message.content.lower() in ['karttest', '/karttest', 'charactertest', '/charactertest']:
         target_channel = message.guild.get_channel(1013170155162972222)  # Get the channel object using the ID
-        if target_channel is not None:
+        if (target_channel is not None) and (target_channel != message.channel) and (message.guild.id == 1013085921647792168): #there may be two checks in here that do the exact same fucking thing but I'm too scared of 2023 Jopes's coding abilities trust it to function properly
             await message.channel.purge(limit=1)
             await message.channel.send(f"Hey {message.author.mention}, please go to {target_channel.mention} to use that command. Thanks!")
-    if message.author.id == 699320094119166064:
-        print("toad gets destroyed and the bot probably crashes")
-        await message.add_reaction("<:Goodshyguy:1066108921821024286>")
-        await message.add_reaction("<:IMG_1443:1049859940459753512>")
-        await message.add_reaction("<:badshyguy:1066108822470537277>")
-        await message.add_reaction("<:bruhbomb:1080233198681870336>")
-        await message.add_reaction("<:cola:1049864677259223050>")
-        await message.add_reaction("<:emoji_4:1050214684738994249>")
-        await message.add_reaction("<:emoji_5:1050214697758097408>")
-        await message.add_reaction("<:emoji_6:1050214712282984498>")
-        await message.add_reaction("<:wafflesdum:1066104213177909258>")
-        await message.add_reaction("<a:luigi:1049864795119177818>")
+    if message.author.id == 783176177162518551:
+        print("id")
+        await message.add_reaction('🐟')
     await bot.process_commands(message)
     
 @bot.event
@@ -162,18 +153,21 @@ async def Char_slash(ctx: discord.ApplicationContext, character: discord.Option(
     
 @bot.slash_command(name="karttest")
 async def first_slash(ctx: discord.ApplicationContext, character: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(GetCharactersNames)), kart: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(GetKartsNames)), wheel: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(GetWheelsNames)), glider: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(GetGlidersNames))): 
-    Aspeed = (CharacterData[character]['Speed']+KartData[kart]['Speed']+WheelData[wheel]['Speed']+GliderData[glider]['Speed'])
-    Aminiturbo = (CharacterData[character]['MiniTurbo']+KartData[kart]['MiniTurbo']+WheelData[wheel]['MiniTurbo']+GliderData[glider]['MiniTurbo'])
-    x = (Aspeed-.75)*4 #speed = x
-    y = (Aminiturbo-.75)*4#miniturbo = y
-    weight = round(((1+(.006*y))*((((.005*y)*((x*.025)+7.45))*((8.9*((y*1)+20))+(8.8*((y*2)+70))+(2.7*((y*3)+120))))+(((8.9*(y*1))+(8.8*(y*2))+(2.7*(y*3)))*(((1.05+(.005*y))*((x*.025)+7.45)))-((x*.025)+7.45))+(((y*1)+15)*((.05*y)*((x*.025)+7.45)))+((y*1)*((.05*y+(1.30))*((x*.025)+7.45)))))+(.88*(8640*(.025*x))))
-    weightpercentile = round((weight-5293)/42.75)
-    embed=discord.Embed(title="KartTest", description=CharacterData[character]['Emojii']+KartData[kart]['Emojii']+WheelData[wheel]['Emojii']+GliderData[glider]['Emojii'], color=0x00ffaa)
-    embed.add_field(name="Parts:", value="Character: " + character + CharacterData[character]['Emojii'] + '   Kart: ' + kart + KartData[kart]['Emojii'] + '   Wheel: ' + wheel + WheelData[wheel]['Emojii'] + '   Glider: ' + glider + GliderData[glider]['Emojii'], inline=False)
-    embed.add_field(name="Score:", value="Speed: " + str(round(x)) + '\nMiniTurbo: ' + str(round(y)) +'\n\n**Weight: ' + str(weight) +'**', inline=True)
-    embed.add_field(name="Percentile: " + str(weightpercentile), value=('\n(out of 100 karts this one would be better than ' + str(weightpercentile) + '\nof them)'), inline=True)
-    embed.set_footer(text="Developed by Jopes on Youtube")
-    await ctx.respond(embed=embed)
+    if(ctx.guild.id == 1013085921647792168 and ctx.channel.id !=1013170155162972222):
+        await ctx.respond(f"Hey {ctx.author.mention}, please go to {ctx.guild.get_channel(1013170155162972222).mention} to use that command. Thanks!")
+    else:
+    	Aspeed = (CharacterData[character]['Speed']+KartData[kart]['Speed']+WheelData[wheel]['Speed']+GliderData[glider]['Speed'])
+    	Aminiturbo = (CharacterData[character]['MiniTurbo']+KartData[kart]['MiniTurbo']+WheelData[wheel]['MiniTurbo']+GliderData[glider]['MiniTurbo'])
+    	x = (Aspeed-.75)*4 #speed = x
+    	y = (Aminiturbo-.75)*4#miniturbo = y
+    	weight = round(((1+(.006*y))*((((.005*y)*((x*.025)+7.45))*((8.9*((y*1)+20))+(8.8*((y*2)+70))+(2.7*((y*3)+120))))+(((8.9*(y*1))+(8.8*(y*2))+(2.7*(y*3)))*(((1.05+(.005*y))*((x*.025)+7.45)))-((x*.025)+7.45))+(((y*1)+15)*((.05*y)*((x*.025)+7.45)))+((y*1)*((.05*y+(1.30))*((x*.025)+7.45)))))+(.88*(8640*(.025*x))))
+    	weightpercentile = round((weight-5293)/42.75)
+    	embed=discord.Embed(title="KartTest", description=CharacterData[character]['Emojii']+KartData[kart]['Emojii']+WheelData[wheel]['Emojii']+GliderData[glider]['Emojii'], color=0x00ffaa)
+    	embed.add_field(name="Parts:", value="Character: " + character + CharacterData[character]['Emojii'] + '   Kart: ' + kart + KartData[kart]['Emojii'] + '   Wheel: ' + wheel + WheelData[wheel]['Emojii'] + '   Glider: ' + glider + GliderData[glider]['Emojii'], inline=False)
+    	embed.add_field(name="Score:", value="Speed: " + str(round(x)) + '\nMiniTurbo: ' + str(round(y)) +'\n\n**Weight: ' + str(weight) +'**', inline=True)
+    	embed.add_field(name="Percentile: " + str(weightpercentile), value=('\n(out of 100 karts this one would be better than ' + str(weightpercentile) + '\nof them)'), inline=True)
+    	embed.set_footer(text="Developed by Jopes on Youtube")
+    	await ctx.respond(embed=embed)
 @bot.event
 async def on_ready():
     bot.add_view(MyView())
